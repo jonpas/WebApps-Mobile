bb499f03e552a69380e3d1d988a2db62d9530d93
jonpas <jonpas33@gmail.com>
2020-01-13 21:03:22 +0100

    Implement required WebView functionalities Load local or hosted page Enable JavaScript and DOM Storage Load all URLs inside WebView instead of opening default device browser Add Refresh button on top of WebView, refreshing current page Override back button to go page back inside WebView instead of closing application

--- app/src/main/java/com/jonpas/webapps/WebViewActivity.kt
+++ app/src/main/java/com/jonpas/webapps/WebViewActivity.kt
@@ -1,12 +1,47 @@
 package com.jonpas.webapps
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_web_view.*
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
 
 class WebViewActivity : AppCompatActivity() {
 
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
-v   super.onCreate(savedInstanceState)
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web_view)
+
+        // Load website
+        //webView.loadUrl("http://10.0.2.2:8000/") // Local Development
+        webView.loadUrl("https://jonpas.eu.pythonanywhere.com/") // PythonAnywhere Hosted
+
+        // Enable JavaScript
+        webView.settings.domStorageEnabled = true
+        webView.settings.javaScriptEnabled = true
+
+        // Load all URLs inside WebView instead of default browser
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                view?.loadUrl(url)
+                return true
+            }
+        }
+
+        // Refresh button
+        refreshButton.setOnClickListener {
+            webView.reload()
+        }
+    }
+
+    // Keep back button inside WebView, going a page back
+    override fun onBackPressed() {
+        when {
+            webView?.canGoBack() == true -> webView.goBack()
+            else -> super.onBackPressed()
+        }
     }
 }
